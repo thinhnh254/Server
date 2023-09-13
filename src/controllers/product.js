7af4b5e16654cbf781497f93921acf806502d822
Thinh <nguyenhungthinh2504@gmail.com>
2023-09-13 20:45:51 +0700

    CRUD product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
+const product = require("../models/product");
 
 const createProduct = asyncHandler(async (req, res) => {
   if (Object.keys(req.body) === 0) {
@@ -21,6 +22,58 @@ const createProduct = asyncHandler(async (req, res) => {
   });
 });
 
+const getProduct = asyncHandler(async (req, res) => {
+  const { pid } = req.params;
+  const product = await Product.findById(pid);
+
+  return res.status(200).json({
+    success: product ? true : false,
+    productData: product ? product : "Can not get product!",
+  });
+});
+
+const getAllProduct = asyncHandler(async (req, res) => {
+  const products = await Product.find();
+
+  return res.status(200).json({
+    success: products ? true : false,
+    productData: products ? products : "Can not get all product!",
+  });
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { pid } = req.params;
+  if (req.body && req.body.title) {
+    req.body.slug = slugify(req.body.title);
+  }
+  const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, {
+    new: true,
+  });
+
+  return res.status(200).json({
+    success: updatedProduct ? true : false,
+    updatedProductStatus: updatedProduct
+      ? updatedProduct
+      : "Can not update product!",
+  });
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const { pid } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(pid);
+
+  return res.status(200).json({
+    success: deletedProduct ? true : false,
+    deletedProductStatus: deletedProduct
+      ? "Success"
+      : "Can not delete product!",
+  });
+});
+
 module.exports = {
   createProduct,
+  getProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
 };
--- src/routes/product.js
+++ src/routes/product.js
@@ -5,4 +5,16 @@ const { verifyAccessToken, isAdmin } = require("../middleware/verifyToken");
 
 router.post("/", [verifyAccessToken, isAdmin], userController.createProduct);
 
+router.put("/:pid", [verifyAccessToken, isAdmin], userController.updateProduct);
+
+router.delete(
+  "/:pid",
+  [verifyAccessToken, isAdmin],
+  userController.deleteProduct
+);
+
+router.get("/", userController.getAllProduct);
+
+router.get("/:pid", userController.getProduct);
+
 module.exports = router;
